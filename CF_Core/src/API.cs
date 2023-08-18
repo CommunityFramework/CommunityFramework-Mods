cec9861548b8c0e644bdb046da8d8776b14197e9
Zipcore <zipcore@googlemail.com>
2023-08-18 14:51:50 +0200

    cleanup

--- CF_Core/src/API.cs
+++ CF_Core/src/API.cs
@@ -2,7 +2,7 @@
 {
     public class API : IModApi
     {
-        public static CF_Log x = new CF_Log("CF_Core", "Timers");
+        public static CF_Log log = new CF_Log("CF_Core", "Timers");
         public void InitMod(Mod _modInstance)
         {
             CF_Timer.TimerStart();
