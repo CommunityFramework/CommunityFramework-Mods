ebdd979589658ef60e8238f03846b16db9a7ee19
Zipcore <zipcore@googlemail.com>
2023-08-20 16:08:08 +0200

    add CF_Array

---
+++ CF_Core/src/NoNamespace/CF_Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CF_Array
+{
+    public static bool ContainsNonNull<T>(T[] items)
+    {
+        foreach (var item in items)
+        {
+            if (item != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
